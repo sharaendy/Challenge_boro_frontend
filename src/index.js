3582ec34994a656bdac0980f89d29a5063e08135
andrey.sh <aash7133@gmail.com>
2022-08-25 19:32:35 +0700

    reset button add

--- src/index.js
+++ src/index.js
@@ -27,76 +27,78 @@ async function app() {
 
   function changeVisibility(eventId) {
     state.uiState.forEach((item) => {
-      const { id } = item;
-      if (eventId === id) {
-        item.isVisible = false;
+      const uiItem = item;
+      if (eventId === item.id) {
+        uiItem.isVisible = false;
       }
     });
   }
 
   function renderUi(cards) {
-    cards.filter(({ isVisible }) => isVisible).map((card) => {
-      const { image, category, timestamp } = card;
+    cards
+      .filter(({ isVisible }) => isVisible)
+      .map((card) => {
+        const { image, category, timestamp } = card;
 
-      const listEl = createNode('li');
-      const cardInfoEl = createNode('div');
-      const buttonEl = createNode('button');
-      const imageEl = createNode('img');
-      const imagePath = `http://contest.elecard.ru/frontend_data/${image}`;
+        const listEl = createNode('li');
+        const cardInfoEl = createNode('div');
+        const buttonEl = createNode('button');
+        const imageEl = createNode('img');
+        const imagePath = `http://contest.elecard.ru/frontend_data/${image}`;
 
-      listEl.classList.add('card');
-      imageEl.classList.add('card-img');
-      imageEl.src = imagePath;
-      cardInfoEl.classList.add('card-info');
-      buttonEl.classList.add('card-btn');
+        listEl.classList.add('card');
+        imageEl.classList.add('card-img');
+        imageEl.src = imagePath;
+        cardInfoEl.classList.add('card-info');
+        buttonEl.classList.add('card-btn');
 
-      cardInfoEl.textContent = 'Yep, just some simple content ecapsulated in this card.';
-      buttonEl.textContent = 'X';
-      buttonEl.setAttribute('id', timestamp);
-      buttonEl.addEventListener('click', (e) => {
-        const eventId = Number(e.target.id);
-        changeVisibility(eventId);
-        cardList.innerHTML = null;
-        renderUi(state.uiState);
+        cardInfoEl.textContent = 'Yep, just some simple content ecapsulated in this card.';
+        buttonEl.textContent = 'X';
+        buttonEl.setAttribute('id', timestamp);
+        buttonEl.addEventListener('click', (e) => {
+          const eventId = Number(e.target.id);
+          changeVisibility(eventId);
+          cardList.innerHTML = null;
+          renderUi(state.uiState);
+        });
+
+        switch (category) {
+          case 'animals':
+            imageEl.alt = 'image with animals';
+            break;
+          case 'business':
+            imageEl.alt = 'image with business attr-s';
+            break;
+          case 'vehicle':
+            imageEl.alt = 'image with vehicle';
+            break;
+          case 'food':
+            imageEl.alt = 'image with food';
+            break;
+          case 'health':
+            imageEl.alt = 'image with health attr-s';
+            break;
+          case 'places':
+            imageEl.alt = 'image with some place';
+            break;
+          case 'science':
+            imageEl.alt = 'image with science attr-s';
+            break;
+          case 'winter':
+            imageEl.alt = 'winter landscape';
+            break;
+          default:
+            imageEl.alt = '';
+            break;
+        }
+
+        appendElement(listEl, imageEl);
+        appendElement(listEl, cardInfoEl);
+        appendElement(listEl, buttonEl);
+        appendElement(cardList, listEl);
+
+        return null;
       });
-
-      switch (category) {
-        case 'animals':
-          imageEl.alt = 'image with animals';
-          break;
-        case 'business':
-          imageEl.alt = 'image with business attr-s';
-          break;
-        case 'vehicle':
-          imageEl.alt = 'image with vehicle';
-          break;
-        case 'food':
-          imageEl.alt = 'image with food';
-          break;
-        case 'health':
-          imageEl.alt = 'image with health attr-s';
-          break;
-        case 'places':
-          imageEl.alt = 'image with some place';
-          break;
-        case 'science':
-          imageEl.alt = 'image with science attr-s';
-          break;
-        case 'winter':
-          imageEl.alt = 'winter landscape';
-          break;
-        default:
-          imageEl.alt = '';
-          break;
-      }
-
-      appendElement(listEl, imageEl);
-      appendElement(listEl, cardInfoEl);
-      appendElement(listEl, buttonEl);
-      appendElement(cardList, listEl);
-
-      return null;
-    });
   }
 
   setUiState();
